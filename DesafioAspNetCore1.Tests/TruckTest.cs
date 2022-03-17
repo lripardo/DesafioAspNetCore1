1615da1bd9b2bc4f167689d82f210805b5061aea
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2022-03-17 18:50:58 -0300

    test: current year or subsequent test

--- DesafioAspNetCore1.Tests/TruckTest.cs
+++ DesafioAspNetCore1.Tests/TruckTest.cs
@@ -11,13 +11,31 @@ public class TruckTest
     {
         var currentYearValidator = new CurrentYear();
         var validYear = DateTime.Now.Year;
-        var invalidYears = new int[] {validYear - 1, validYear + 1};
+        var invalidYears = new[] {validYear - 1, validYear + 1};
 
         Assert.True(currentYearValidator.IsValid(validYear));
 
         foreach (var year in invalidYears)
         {
             Assert.False(currentYearValidator.IsValid(year));
+        }
+    }
+
+    [Fact]
+    public void CurrentYearOrSubsequentTest()
+    {
+        var currentYearOrSubsequentValidator = new CurrentYearOrSubsequent();
+        var validYears = new[] {DateTime.Now.Year, DateTime.Now.Year + 1};
+        var invalidYears = new[] {validYears[0] - 1, validYears[1] + 1};
+
+        foreach (var year in validYears)
+        {
+            Assert.True(currentYearOrSubsequentValidator.IsValid(year));
+        }
+
+        foreach (var year in invalidYears)
+        {
+            Assert.False(currentYearOrSubsequentValidator.IsValid(year));
         }
     }
 }
